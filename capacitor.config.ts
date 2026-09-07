e0887a7de2b79b6438bf404c1d8cddeaaaa4b20a
RUWANTHA-OFC <lasithruwantha3@gamil.com>
2026-09-07 11:44:01 +0530

    fix all

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.foodsay.ruwantha',
   appName: 'food say',
   webDir: 'dist',
+  server:{
+    androidScheme: "https"
+  },
   plugins: {
     LocalNotifications: {
       smallIcon: "ic_stat_app_icon",
